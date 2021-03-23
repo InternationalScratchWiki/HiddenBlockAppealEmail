c3b7a0b02190727e03297022eafbc24a0cd646c5
Jacob G <jacob-x@jvvgindustries.com>
2021-03-23 23:47:14 +0000

    disable parser caching and use better null fallback logic

--- BlockAppealEmail.php
+++ BlockAppealEmail.php
@@ -7,10 +7,13 @@ class BlockAppealEmail {
 	public static function onBlockAppealEmail(?string $body, array $args, Parser $parser, PPFrame $frame) : string {
 		global $wgBlockAppealEmail;
 		
+		//disable caching so that the block information is truly up to date
+		$parser->getOutput()->updateCacheExpiry(0);
+		
 		if ($parser->getUser()->isBlocked()) {
 			return $wgBlockAppealEmail;
 		} else {
-			return $body || '';
+			return empty($body) ? '' : $body;
 		}
 	}
 }
