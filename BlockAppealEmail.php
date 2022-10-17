98fbf4b7ea6729d4b740f79475f93501f75df82c
apple502j <33279053+apple502j@users.noreply.github.com>
2022-10-18 02:06:36 +0900

    Use new hook system (#2)

--- BlockAppealEmail.php
+++ BlockAppealEmail.php
@@ -1,19 +1,22 @@
 <?php
-class BlockAppealEmail {
-	public static function onParserFirstCallInit(Parser $parser) : void {
+
+use MediaWiki\Hook\ParserFirstCallInitHook;
+
+class BlockAppealEmail implements ParserFirstCallInitHook {
+	public function onParserFirstCallInit($parser) {
 		$parser->setHook('blockappealemail', [self::class, 'onBlockAppealEmail']);
 	}
-	
-	public static function onBlockAppealEmail(?string $body, array $args, Parser $parser, PPFrame $frame) : string {
+
+	public static function onBlockAppealEmail(?string $body, array $args, Parser $parser, PPFrame $frame): string {
 		global $wgBlockAppealEmail;
-		
+
 		//disable caching so that the block information is truly up to date
 		$parser->getOutput()->updateCacheExpiry(0);
-		
+
 		if ($parser->getUser()->getBlock()) {
 			return $wgBlockAppealEmail;
 		} else {
-			return empty($body) ? '' : $parser->recursiveTagParse($body);
+			return $body === null ? '' : $parser->recursiveTagParse($body);
 		}
 	}
 }
